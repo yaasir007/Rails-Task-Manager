87507fcdc6a743c2c4b3c51770cae4dfe746e007
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-03 22:16:57 +0400

    Index Completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task = Task.find(params[:id])
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+end
